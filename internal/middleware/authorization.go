e96ce9e2f56384465742beaedaf2a4989d85643d
Krishna <krishnakadam013@gmail.com>
2026-01-18 23:00:43 +0530

    WIP: Add authorization

--- internal/middleware/authorization.go
+++ internal/middleware/authorization.go
@@ -11,3 +11,11 @@ import (
 
 var UnAuthorizedError = errors.New("Invalid username or token.")
 
+func Authorization(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {	
+
+		var username string = r.URL.Query().Get("username")
+		var token = r.Header.Get("Authorization")
+		var err error
+
+	}
