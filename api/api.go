2e39666f196ad96a75eea64152f6f169321220a1
Krishna <krishnakadam013@gmail.com>
2026-01-22 23:51:22 +0530

    WIP: api/api.go

--- api/api.go
+++ api/api.go
@@ -24,3 +24,16 @@ type Error struct {
 	//Error message
 	Message string
 }
+
+//-------
+func writeError(w http.ResponseWriter, message string, code int) {
+	resp := Error{
+		Code:    code,
+		Message: message,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	json.NewEncoder(w).Encode(resp)
+}
